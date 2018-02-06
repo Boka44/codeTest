bc16e1cdf5a1c7e8da7ab14412297b6b3173d0a2
Boka44 <Guitar101!>
2018-02-06 13:17:32 -0800

    added input validation

--- server.js
+++ server.js
@@ -13,37 +13,42 @@ app.post('/', function(req, res){
 	let numberOfDays = req.body.numberOfDays;
 	endDate.setDate(startDate.getDate() + numberOfDays); 
 
-	let totalSaved = 0;
+	if(numberOfDays < 1 || !Number.isInteger(numberOfDays)) {
+		res.send("Please put a number greater than zero for the number of days.")
+		res.end()
+	} else {
 
-	const first7 = 0.05;
-	const second7 = 0.1;
-	const third7 = 0.15;
-	const fourth7 = 0.2;
-	const remainingDays = 0.25;
+		let totalSaved = 0;
 
-	for(let tempDate = startDate; tempDate < endDate; tempDate.setDate(startDate.getDate() + 1)) {
-	    
-	    if(tempDate.getDay() === 0 || tempDate.getDay() === 6) {
-	      continue;
-	    } else if(tempDate.getDate() < 8) {
-	      totalSaved += first7;
-	    } else if(tempDate.getDate() > 7 && tempDate.getDate() < 15) {
-	      totalSaved += second7;
-	    } else if(tempDate.getDate() > 14 && tempDate.getDate() < 22) {
-	      totalSaved += third7;
-	    } else if(tempDate.getDate() > 21 && tempDate.getDate() < 29) {
-	      totalSaved += fourth7;
-	    } else {
-	      totalSaved += remainingDays;
-	    }
-	   
+		const first7 = 0.05;
+		const second7 = 0.1;
+		const third7 = 0.15;
+		const fourth7 = 0.2;
+		const remainingDays = 0.25;
+
+		for(let tempDate = startDate; tempDate < endDate; tempDate.setDate(startDate.getDate() + 1)) {
+		    
+		    if(tempDate.getDay() === 0 || tempDate.getDay() === 6) {
+		      continue;
+		    } else if(tempDate.getDate() < 8) {
+		      totalSaved += first7;
+		    } else if(tempDate.getDate() > 7 && tempDate.getDate() < 15) {
+		      totalSaved += second7;
+		    } else if(tempDate.getDate() > 14 && tempDate.getDate() < 22) {
+		      totalSaved += third7;
+		    } else if(tempDate.getDate() > 21 && tempDate.getDate() < 29) {
+		      totalSaved += fourth7;
+		    } else {
+		      totalSaved += remainingDays;
+		    }
+		   
+	 	}
+	 	if(totalSaved === 0) {
+			res.send("Date is in incorrect format, try one of these options: March 15 2018 || 5/15/2018 || 2018-05-15 || 15 March 2018. <br> Note: Money is not saved on Saturdays or Sundays, be sure to include one weekday to see a result.")
+		} else{
+			res.send('$'+ totalSaved.toFixed(2));
+		}
  	}
- 	if(totalSaved === 0) {
-		res.send("Date is in incorrect format, try one of these options: March 15 2018 || 5/15/2018 || 2018-05-15 || 15 March 2018. <br> Note: Money is not saved on Saturdays or Sundays, be sure to include one weekday to see a result.")
-	} else{
-		res.send('$'+ totalSaved.toFixed(2));
-	}
- 	
 })
 
 app.listen(PORT, function() {
